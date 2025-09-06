09832d9339b334feb1e633e368841a55c3816b67
OkoyeEmmanuel <okoye.tochukwu.emmanuel@gmail.com>
2025-09-06 17:08:25 +0200

    update keylogger.py
    the 'with' keyword handles the closing of the file

--- keylogger.py
+++ keylogger.py
@@ -3,3 +3,6 @@
 f= open("log.txt","a")
 f.write("My Name is Emmanuel\n")
 f.close()
+
+with open("log_1.txt","a") as f:
+    f.write("when using the 'with' you do not need to close the file, its automatically does it for you\n")

cd115c71d2f40f9fa245bcbee72ed279efdf8d1d
OkoyeEmmanuel <okoye.tochukwu.emmanuel@gmail.com>
2025-09-09 23:13:17 +0200

    Update keylogger.py
    Just experimented using the mouse and the keyboard, but this is custom for the laptop dimensions for my web browser application. Really cool

--- keylogger.py
+++ keylogger.py
@@ -1,14 +1,25 @@
-from pynput.mouse import Controller
-from pynput.keyboard import Controller
+from pynput.mouse import Controller as MouseController, Button
+from pynput.keyboard import Controller as KeyboardController, Key
+
 
 def controlMouse():
-    mouse = Controller()
-    mouse.position = (102, 20)
+    mouse = MouseController()
+    mouse.position = (860, 20)
+    mouse.click(Button.left, 2)
     print(f"Mouse moved to: {mouse.position}")
 
 def controlKeyboard():
-    keyboard = Controller()
-    keyboard.type('My Name is Emmanuel, Growing!!') 
+    keyboard = KeyboardController()
+    keyboard.press(Key.backspace)
+    keyboard.release(Key.backspace)
+    keyboard.type('www.google.com') 
 
 
+def enter():
+    keyboard = KeyboardController()
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+
+controlMouse()
 controlKeyboard()
+enter()

